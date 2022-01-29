473a75ef4629a2ea8c6d35ef4b393081a97ac182
jayson7 <lexxiijoo70@gmail.com>
2022-01-29 17:02:18 +0100

    tuberz

--- tubersproject/apps/views.py
+++ tubersproject/apps/views.py
@@ -8,12 +8,20 @@ from pytube import *
 
 def Home(request):
     # checking whether request.method is post or not
+    context = {}
     if request.method == 'POST':
        
         # getting link from frontend
         link = request.POST['link']
         video = YouTube(link)
- 
+        # title
+        video_title = video.title
+        # thumbnail
+        thumbnail_url = video.thumbnail.url
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # highest resolution
+        highest_resolution = video.streams.get_highest_resolution()
+        highest_resolution_download = highest_resolution.download()
         # setting video resolution
         stream = video.streams.get_lowest_resolution()
         print(stream)  
