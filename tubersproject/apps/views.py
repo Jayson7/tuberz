b28e50303f4bf0361af37a07278bd125ddd01149
jayson7 <lexxiijoo70@gmail.com>
2022-01-30 06:51:02 +0100

    tuberz

--- tubersproject/apps/views.py
+++ tubersproject/apps/views.py
@@ -2,7 +2,6 @@ from django.http import request
 from django.shortcuts import render, redirect
 import pytube 
 from pytube import *
-
 # Create your views here.
 
 
@@ -18,7 +17,62 @@ def Home(request):
         # title
         video_title = video.title
         # thumbnail
-        thumbnail_url = video.thumbnail.url
+        thumbnail_urls = video.thumbnail_url
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        # highest resolution
+        highest_resolution = video.streams.get_highest_resolution()
+        highest_resolution_download = highest_resolution.download()
+        # smallest resolution
+        smallest_resolution = video.streams.get_lowest_resolution()
+        smallest_resolution_download = smallest_resolution.download()
+        print(highest_resolution)  
+        # downloads video
+        # stream.download()
+ 
+        # returning HTML page
+        return render(request, 'index.html')
+    
+    return render(request, 'index.html')
+
+def Small(request):
+    # checking whether request.method is post or not
+    context = {}
+    if request.method == 'POST':
+       
+        # getting link from frontend
+        link = request.POST['link']
+        video = YouTube(link)
+        # title
+        video_title = video.title
+        # thumbnail
+        print(request.path)
+        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+        
+        # smallest resolution
+        
+        smallest_resolution = video.streams.get_lowest_resolution()
+        
+          
+        # downloads video
+        # stream.download()
+ 
+        # returning HTML page
+        return render(request, 'index.html')
+    
+    return render(request, 'index.html')
+
+def Large(request):
+    # checking whether request.method is post or not
+    context = {}
+    if request.method == 'POST':
+       
+        # getting link from frontend
+        link = request.POST['link']
+        video = YouTube(link)
+        # title
+        video_title = video.title
+        # thumbnail
+        thumbnail_urls = video.thumbnail_url
         # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
         # highest resolution
         highest_resolution = video.streams.get_highest_resolution()
--- tubersproject/tubersproject/urls.py
+++ tubersproject/tubersproject/urls.py
@@ -7,4 +7,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.Home, name='home'),
+    path('', views.Large, name='large'),
+    path('', views.Small, name='small'),
+    
 ]
