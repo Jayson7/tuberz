1a16660d6baf44252083d4abffe1f44d2a464de8
jayson7 <lexxiijoo70@gmail.com>
2022-01-30 15:22:45 +0100

    tuberz

--- tubersproject/apps/views.py
+++ tubersproject/apps/views.py
@@ -1,5 +1,6 @@
 from django.http import request
 from django.shortcuts import render, redirect
+from django.contrib import messages
 # from django.urls import resolve
 
 import pytube 
@@ -13,33 +14,40 @@ def Home(request):
     
     return render(request, 'index.html' ) 
 
+
+
 def Small(request):
     # current_url = resolve(request.path_info).url_name
     # checking whether request.method is post or not
+    
     context = {}
-    if request.method == 'POST':
-        print(current_url)
+    try:
+        if request.method == 'POST':
         
-        # getting link from frontend
-        link = request.POST['link']
-        video = YouTube(link)
-        # title
-        video_title = video.title
-        # thumbnail
-        
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        
-        # smallest resolution
-        
-        smallest_resolution = video.streams.get_lowest_resolution()
-        smallest_resolution.download()
-          
-        # downloads video
-        # stream.download()
- 
-        # returning HTML page
-        return redirect('/')
+            
+            # getting link from frontend
+            link = request.POST['link']
+            video = YouTube(link)
+            # title
+            video_title = video.title
+            # thumbnail
+            
+            # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+            
+            # smallest resolution
+            
+            smallest_resolution = video.streams.get_lowest_resolution()
+            smallest_resolution.download()
+            
+            # downloads video
+            # stream.download()
+    
+            # returning HTML page
+            
+            return render(request, 'small.html')
 
+    except:
+        redirect ('/')
     
     return render(request, 'small.html')
 
@@ -64,8 +72,10 @@ def Large(request):
         
     
             # returning HTML page
-        
-            return redirect('/')
+            messages.success(request, 'Your file is currently downloading', extra_tags='alert')
+            return render(request, 'large.html')
+
     except:
-        pass
+        messages.warning(request, 'Please correct the error below.')
+        redirect ('/')
     return render(request, 'large.html')
